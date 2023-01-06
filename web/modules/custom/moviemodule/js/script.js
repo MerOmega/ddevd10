438a5608ba51c3c778f725f2038aeb610885cf02
MerOmega <insaurralde30@gmail.com>
2023-01-06 13:43:46 -0300

    js added with bugs

--- web/modules/custom/moviemodule/js/script.js
+++ web/modules/custom/moviemodule/js/script.js
@@ -1,23 +1,47 @@
-const card = document.querySelectorAll('.card');
-const searchMovie=async (id)=>{
+let card = document.querySelectorAll('.card');
+
+let searchMovie=async (id)=>{
   let query = await fetch(
     "https://api.themoviedb.org/3/person/"+id+"?api_key=7dddb325f57844046ce6315eeb90960f&language=en-US");
   let jsonItem = await query.json();
   return jsonItem;
 }
 
-card.forEach(box => {
-  box.addEventListener('click', ()=> {
-    element = box.querySelector(".relevant-movies");
-      if(!element.classList.contains("profile")){
-        element.classList.add("profile");
-        element.classList.add("uncover");
-        searchMovie(box.id).then(data=>{
-            let createNode = document.createElement("p");
-            let node = document.createTextNode(data.biography);
-            createNode.appendChild(node);
-            element.appendChild(createNode);
-        });
-    }
-  })
+const innerhtml = (name,pofile)=>{
+    return (`
+    <div id="popup" class="pop-container container">
+      <div class="popup" id="popup">
+        <div class="popup-header">
+        <h1> ${name} </h1>
+        <button id="close-popup">&times;</button>
+        </div> 
+        <p>${pofile}</p>
+      </div>
+     </div>
+    `);
+}
+
+
+let invoke = async (element, box) => {
+  return searchMovie(box.id).then((data) => {
+    element.innerHTML += innerhtml(data.name, data.biography);
+    button = element.querySelector("#close-popup");
+    button.addEventListener("click", () => {
+      popup = document.querySelector("#popup");
+      popup.remove();
+    });
+  });
+};
+
+let createPopupPromise = (element, box) => {
+  return new Promise((resolve) => {
+    resolve(invoke(element, box));
+  });
+};
+
+card.forEach((box) => {
+  box.addEventListener("click", async () => {
+    element = document.querySelector(".grid");
+    await createPopupPromise(element, box);
+  });
 });
---
+++ web/sites/default/files/php/twig/63b84dd4bc731_my-template.html.twig_jGuApADd6A5ZVw62lxFB7pw2j/tfjQ9BxVu6kaizfz6AZCJfe8uu5xa2aIb1a5J-3dKlw.php
@@ -0,0 +1,150 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* modules/custom/moviemodule/templates/my-template.html.twig */
+class __TwigTemplate_8a6557471b9cad7206a2bac6beb3bb34 extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->extensions['Drupal\Core\Template\TwigExtension']->attachLibrary("moviemodule/movielibrary"), "html", null, true);
+        echo "
+";
+        // line 2
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->extensions['Drupal\Core\Template\TwigExtension']->attachLibrary("moviemodule/bootstrap"), "html", null, true);
+        echo "
+
+<h1 id=\"api-title\">List of Actors/Director</h1>
+</br>
+
+
+<div class=\"grid\">
+  ";
+        // line 9
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable(($context["test_var"] ?? null));
+        foreach ($context['_seq'] as $context["_key"] => $context["value"]) {
+            // line 10
+            echo "    <div class=\"card\" id=\"";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, $context["value"], "id", [], "any", false, false, true, 10), 10, $this->source), "html", null, true);
+            echo "\">
+      ";
+            // line 11
+            if ( !twig_test_empty(twig_get_attribute($this->env, $this->source, $context["value"], "profile_path", [], "any", false, false, true, 11))) {
+                // line 12
+                echo "      <div class=\"profile-image card-img-top\">
+        <img style=\"max-width: 150px;\" src=\"https://image.tmdb.org/t/p/w500";
+                // line 13
+                echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, $context["value"], "profile_path", [], "any", false, false, true, 13), 13, $this->source), "html", null, true);
+                echo "\">
+      </div>
+      ";
+            }
+            // line 16
+            echo "      <div class=\"card-title\">
+        <h2> ";
+            // line 17
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, $context["value"], "name", [], "any", false, false, true, 17), 17, $this->source), "html", null, true);
+            echo "</h2>
+      </div>
+      <div class=\"role\">
+        <p> ";
+            // line 20
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, $context["value"], "known_for_department", [], "any", false, false, true, 20), 20, $this->source), "html", null, true);
+            echo "</p>
+      </div>
+      <div class=\"card-body\">
+
+        <a class=\"button btn btn-primary\" href=\"/movieapi/actor/create/";
+            // line 24
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, $context["value"], "id", [], "any", false, false, true, 24), 24, $this->source), "html", null, true);
+            echo "\">Create and edit</a>
+      </div>
+    </div>
+  ";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['value'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 28
+        echo "</div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "modules/custom/moviemodule/templates/my-template.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  99 => 28,  89 => 24,  82 => 20,  76 => 17,  73 => 16,  67 => 13,  64 => 12,  62 => 11,  57 => 10,  53 => 9,  43 => 2,  39 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "modules/custom/moviemodule/templates/my-template.html.twig", "/var/www/html/web/modules/custom/moviemodule/templates/my-template.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("for" => 9, "if" => 11);
+        static $filters = array("escape" => 1);
+        static $functions = array("attach_library" => 1);
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['for', 'if'],
+                ['escape'],
+                ['attach_library']
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
