8f30059d6fbd064063aeda66eec0146de158686d
MerOmega <insaurralde30@gmail.com>
2023-01-16 20:56:11 -0300

    custom theme

---
+++ web/sites/default/files/php/twig/63c5e372b6636_region.html.twig_e3KmK9vUrKAKJWPLej6BbfSJC/dc443AAZvZr2TdWmuoA0Hut1dIEauBOH-PYnBLY-hEk.php
@@ -0,0 +1,131 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/custom/movietheme/templates/layout/region.html.twig */
+class __TwigTemplate_fcbd9d2f0ea55fe6ecae21d38b1f74fe extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 15
+        echo "<div class=\"";
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["region"] ?? null), 15, $this->source), "html", null, true);
+        echo " p-3 text-bg-dark\">
+";
+        // line 16
+        if (($context["content"] ?? null)) {
+            // line 17
+            echo "  <div";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["attributes"] ?? null), 17, $this->source), "html", null, true);
+            echo " class=\"";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["region"] ?? null), 17, $this->source), "html", null, true);
+            echo "-subitems\">
+    ";
+            // line 18
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["content"] ?? null), 18, $this->source), "html", null, true);
+            echo "
+  </div>
+";
+        }
+        // line 21
+        echo "</div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/custom/movietheme/templates/layout/region.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  59 => 21,  53 => 18,  46 => 17,  44 => 16,  39 => 15,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("{#
+/**
+ * @file
+ * Theme override to display a region.
+ *
+ * Available variables:
+ * - content: The content for this region, typically blocks.
+ * - attributes: HTML attributes for the region <div>.
+ * - region: The name of the region variable as defined in the theme's
+ *   .info.yml file.
+ *
+ * @see template_preprocess_region()
+ */
+#}
+<div class=\"{{ region }} p-3 text-bg-dark\">
+{% if content %}
+  <div{{ attributes }} class=\"{{ region }}-subitems\">
+    {{ content }}
+  </div>
+{% endif %}
+</div>
+", "themes/custom/movietheme/templates/layout/region.html.twig", "/var/www/html/web/themes/custom/movietheme/templates/layout/region.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("if" => 16);
+        static $filters = array("escape" => 15);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['if'],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
